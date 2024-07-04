aa963a394ba6e21d681238048715f830f8048339
Sagar Patil <sagar.patil.contractor@move.com>
2024-07-05 01:15:41 +0530

    JS notes updated

--- JavaScript/#0 Data Types.js
+++ JavaScript/#0 Data Types.js
@@ -14,14 +14,17 @@
 
 // console.log(b); // present in global scope with undefined
 // var b = 3; 
+console.log("\n\n---------------------")
 console.log("____________");
 // let c = 5;
 // let c = 4;  // SyntaxError: Identifier 'c' has already been declared
+console.log("\n\n---------------------")
 console.log("____________");
 // const d ;   //SyntaxError: 'const' declarations must be initialized.
 const d = 5;
 // d = 11;  // TypeError: Assignment to constant variable.
 
+console.log("\n\n---------------------")
 console.log("______#2 Scopes______");
 // #2 Scopes:
 // What is block scope?
@@ -42,6 +45,7 @@ let h = 12;     // script scope
 console.log(e, f); // 10 11
 // console.log(g);  // ReferenceError: g is not defined
 
+console.log("\n\n---------------------")
 console.log("__________#3 typeof__________");
 
 let score = "33";
@@ -108,17 +112,8 @@ console.log(Boolean(1n));       // true
 console.log(Boolean(-1n));      // true
 
 
-
-console.log("________String________");
-let num = 1;
-let strNum = String(num);
-console.log(typeof (strNum));
-console.log(String(null));              // null
-console.log(typeof (String(null)));     // string
-console.log(String(undefined));         // undefined
-// console.log(Str);
-
-console.log("_____String to Number conversion_____");
+console.log("\n\n---------------------")
+console.log("_____conversion/typecasting_____");
 let fname = "Sagar", lname = " Patil";
 console.log(fname + lname);
 
@@ -141,6 +136,7 @@ no1 = no2 = no3 = 2 + 2;
 console.log(no1, no2, no3);
 
 
+console.log("\n\n---------------------")
 console.log("____Comparison______");
 console.log("2" > 1);   // true
 console.log("02" > 1);  // true
@@ -150,6 +146,7 @@ console.log("2" === 2);  // false // values are same but data types are differen
 console.log("null > 0: ", null > 0);  // false
 console.log(null == 0);     // false
 console.log(null >= 0);     // true // here, null get converted into 0.
+console.log(null < 1);     // true // here, null get converted into 0.
 console.log(null === null); // true
 // undefined always false for above 3 scenario's
 
@@ -157,6 +154,7 @@ console.log("Strict check:");
 console.log("2" == 2);
 console.log("2" === 2);
 
+// ----------------------------------------
 /* Data Types:
     - 7 Primitive:  // store on Stack Memory
         - String
@@ -164,17 +162,19 @@ console.log("2" === 2);
         - Boolean
         - null
         - undefined
-        - Symbol
-        - BigInt
+        - Symbol: (Symbol(111))
+        - BigInt: (123n)
     - Reference (Non-Primitive);    // store on heap memory and reference store on stack.
-        - Array
-        - Object
-        - Functions
+        - Array: [1,2,"c",null]
+        - Object: {name:"sagar", gender: "male"}
+        - Functions: function(){}
         - ...
 // Typescript:
     const num:number = 10;
 */
 
+
+console.log("\n\n---------------------")
 console.log("______Symbol_______");
 /*
 Symbol is a unique and immutable data type that is primarily used to create unique identifiers for objects.
@@ -184,28 +184,43 @@ Even if two Symbol values are created with the same description, they are guaran
 const id = Symbol('123');
 const anotherId = Symbol('123');
 console.log("id:", id, "anotherId:", anotherId, "===:", id === anotherId);  // id: Symbol(123) anotherId: Symbol(123) ===: false
+const student = {name: "sagar", age: 11}
+console.log(student === student);   // true
+console.log(student == Symbol(student));   // false
+console.log(student != Symbol(student));   // true
 
+console.log("\n\n---------------------")
+console.log("______bigint_______");
 const bigNumer = 34256636454556634343434343n;
 console.log(bigNumer);
 console.log(typeof bigNumer);   // bigint
 
-console.log("______String_______");
+console.log("\n\n---------------------")
+console.log("________String________");
+let num = 1;
+let strNum = String(num);
+console.log(typeof (strNum));
+console.log(String(null));              // null
+console.log(typeof (String(null)));     // string
+console.log(String(undefined));         // undefined
+console.log(String(true));              // true
+
 
 const myname = "sagar";
 const repoCount = 50;
 console.log(`Hello ${myname}, repo count is: ${repoCount}`);
 
-console.log(myname[0]);
+console.log(myname[0]);     // s
 
 const mystr = new String("abcdef");
-console.log(mystr[0]);
+console.log(mystr[0]);      // a
 console.log(mystr.__proto__);
-console.log(mystr.length);
+console.log(mystr.length);  // 6
 console.log(mystr.toUpperCase());
-console.log(mystr.charAt(3));
+console.log(mystr.charAt(3));   // d
 
 console.log(mystr.indexOf("d")); // 3
-console.log(mystr.indexOf("t")); // -1
+console.log(mystr.indexOf("t")); // -1  // not present
 
 console.log(mystr.substring(0, 4)); // abcd
 console.log(mystr.substring(2, 4)); // cd
@@ -217,24 +232,29 @@ console.log(mystr.slice(-4, 4)); // cd
 // -6 -5 -4 -3 -2 -1
 // 0  1  2  3  4  5
 
-console.log("   abc   ".trim());
-console.log("   abc   ".trimStart());
-console.log("   abc   ".trimEnd());
+console.log("   abc   ".trim());        // "abc"
+console.log("   abc   ".trimStart());   // "abc   "
+console.log("   abc   ".trimEnd());     // "   abc"
 
 
-console.log("sagar.patil".replace(".", " "));
-console.log("sagar.patil".includes(".p"));
+console.log("sagar.patil".replace(".", " "));   /// sagar patil
+console.log("sagar.patil".includes(".p"));  // true
 
 console.log("sagar.b.patil".split("."));    // [ 'sagar', 'b', 'patil' ]
 console.log("sagar.b.patil".split(".", 2)); // [ 'sagar', 'b' ]
 
 
+console.log("\n\n---------------------")
 console.log("____Numbers____");
 const score1 = 400;
 const balance = new Number(100);
 const otherNumber = 123.4567;
 console.log(score1);    // 200
 console.log(balance);   // [Number: 100]
+
+console.log(balance === 100);   // false
+console.log(balance == 100);   // true
+
 
 console.log(balance.toString().length);
 console.log(balance.toFixed(2)); // 100.00
@@ -248,12 +268,20 @@ console.log(no.toLocaleString('en-IN'));    // Indian standart: 10,00,000
 
 console.log(Number.MAX_VALUE);  // 1.7976931348623157e+308
 console.log(Number.MIN_VALUE);  // 5e-324
+console.log(Number.MAX_SAFE_INTEGER);    // 9007199254740991
+console.log(Number.MIN_SAFE_INTEGER);    // -9007199254740991
+console.log(Number.POSITIVE_INFINITY);  // Infinity
 console.log(Number.NEGATIVE_INFINITY);  // -Infinity
 console.log(Number.EPSILON);    // 2.220446049250313e-16
+console.log(Number.NaN);    // NaN
+console.log(NaN);           // NaN
+console.log();
 
+console.log("\n\n---------------------")
 console.log("____Math____");
 console.log(Math.abs(-4));      // 4
 console.log(Math.abs(4));       // 4
+
 console.log(Math.round(5.4));   // 5
 
 console.log("ceil:", Math.ceil(3.9));   // 4
@@ -264,10 +292,10 @@ console.log("floor:", Math.floor(3.9));   // 3
 console.log(Math.floor(4.1));   // 4
 console.log(Math.floor(4.0));   // 4
 
-console.log("max:", Math.max(4, 5, 6, 2));
-console.log("min:", Math.min(4, 5, 6, 2));
+console.log("max:", Math.max(4, 5, 6, 2));  // 6
+console.log("min:", Math.min(4, 5, 6, 2));  // 2
 
-console.log(Math.random()); // 0 to 1
+console.log(Math.random()); // 0.00000 00000 00000 to 1
 console.log(Math.random() * 10); // 0 to 10
 console.log((Math.floor(Math.random() * 10)) + 1); // 1 to 10
 
@@ -276,6 +304,7 @@ console.log((Math.floor(Math.random() * (max - min + 1)) + min)); // 10 to 20
 
 
 
+console.log("\n\n---------------------")
 console.log("____Date______");
 let date = new Date();
 
@@ -285,7 +314,11 @@ console.log(date.toString());       // Tue Mar 12 2024 03:41:49 GMT+0530 (India 
 console.log(date.toDateString());   // Tue Mar 12 2024
 console.log(date.toTimeString());   // 03:42:50 GMT+0530 (India Standard Time)
 console.log(date.toLocaleString()); // 3/12/2024, 3:41:49 AM
+console.log(date.toISOString());    // 2024-07-04T19:06:59.324Z
+console.log(date.toISOString() > new Date("2023-07-04T19:06:59.324Z").toISOString());    // true
 
+console.log(new Date("3/12/2024, 3:41:49 AM"));   // Tue Mar 12 2024 03:41:49 GMT+0530 (India Standard Time)
+console.log(new Date("") );   // Invalid Date
 let newDate = new Date("2024", 0, 26);  // month starts from 0 = January
 let newDateTime = new Date(2024, "0", "26", "14", 55, "45");  // month starts from 0 = January
 console.log("newDate:", newDate.toLocaleString());          // 1/26/2024, 12:00:00 AM
@@ -293,12 +326,12 @@ console.log("newDateTime:", newDateTime.toLocaleString());  // newDateTime: 1/26
 
 var newDate2 = new Date("2024-01-25");  // YYYY-MM-DD
 console.log(newDate2);  // 2024-01-25T00:00:00.000Z
-let newDate3 = new Date("01-14-2024");  // MM-DD-YYYY // 024-01-13T18:30:00.000Z
+let newDate3 = new Date("01-14-2024");  // MM-DD-YYYY // 2024-01-13T18:30:00.000Z
 console.log(newDate3);
 
 let myTimeStamp = Date.now();
-console.log("myTimeStamp:", myTimeStamp);   // 1710196046602 ans in mili sec.
-console.log(date.getTime());   // ans in mili sec.  
+console.log("myTimeStamp:", myTimeStamp);   // 1720120400329 ans in mili sec.
+console.log(date.getTime());   // ans in mili sec.  // 1720120400313  
 console.log(myTimeStamp - date.getTime()); // 6, ans in mili sec, time difference
 
 console.log(Math.floor(Date.now() / 1000));    // 1710196046, convert into seconds
@@ -311,6 +344,7 @@ console.log(date.toLocaleString("default", {
 }));    // Tuesday
 
 
+console.log("\n\n---------------------")
 console.log("______Arrays______");
 
 var myarr = [0, 1, 2, 3, 4]
@@ -322,9 +356,10 @@ console.log(typeof myarr, typeof myArrObj); // object object
 myarr.push(5);
 myarr.push(6);
 myarr.pop();
-myarr.unshift(9);   // add at start
+console.log(myarr.unshift(9));  // 7 (returns length of array)   // add at start
+// myarr.unshift(99,88,77);   // add at start
 console.log(myarr); // [ 9, 0, 1, 2, 3, 4, 5 ]
-myarr.shift();      // remove start/first element
+console.log(myarr.shift());     // 9    // remove start/first element
 console.log(myarr); // [ 0, 1, 2, 3, 4, 5 ]
 myarr.unshift(2);   // add at start
 console.log(myarr.includes(4)); // true
@@ -335,41 +370,41 @@ console.log(myarr.join());  // "2,0,1,2,3,4,5"
 // slice vs splice
 console.log("slice vs splice");
 console.log(myArrObj);      // [ 1, 2, 3, 4, 5 ]
-console.log(myArrObj.slice(1, 3));   // [ 2, 3 ] // return slice of code.
+console.log(myArrObj.slice(1, 3));   // [ 2, 3 ] // return slice of data.
 console.log(myArrObj);  // [ 1, 2, 3, 4, 5 ]
 
-console.log(myArrObj.splice(1, 3)); // [ 2, 3, 4 ] // delete slice and return slice of code.
+console.log(myArrObj.splice(1, 3)); // [ 2, 3, 4 ] // return slice of data and delete slice from original data
 console.log(myArrObj);  // [ 1, 5 ]
 
 const marvel_heroes = ["thor", "Ironman", "SpiderMan"];
 const dc_heroes = ["Superman", "Batman", "WonderWomen"];
 
 // marvel_heroes.push(dc_heroes);
-// console.log(marvel_heroes[0]);
-// console.log(marvel_heroes[3][1]);
+console.log(marvel_heroes[0]);          // thor
+// console.log(marvel_heroes[3][1]);    // Java: ArrayIndexOutOfBound   // TypeError: Cannot read properties of undefined (reading '1')
+console.log(marvel_heroes[0][1]);       // h
 
 var all_heroes = marvel_heroes.concat(dc_heroes);
 console.log(all_heroes);    // [ 'thor', 'Ironman', 'SpiderMan', 'Superman', 'Batman', 'WonderWomen' ]
 
-all_heroes = [...marvel_heroes, ...dc_heroes];
+all_heroes = [...marvel_heroes, ...dc_heroes];  // using spread operator
 
 var depthArr = [1, 2, 3, [4, 5], 6, [7, 8, [9, 10, [11, 12,]]], 13];
-console.log(depthArr.flat(1)); // [ 1, 2, 3, 4, 5, 6, 7, 8, [ 9, 10, [ 11, 12 ] ], 13 ]
-console.log(depthArr.flat(Infinity)); // [ 1, 2, 3,  4,  5,  6, 7, 8, 9, 10, 11, 12, 13 ]
+console.log("flat(1):", depthArr.flat(1)); // [ 1, 2, 3, 4, 5, 6, 7, 8, [ 9, 10, [ 11, 12 ] ], 13 ]
+console.log("flat(Infinity):", depthArr.flat(Infinity)); // [ 1, 2, 3,  4,  5,  6, 7, 8, 9, 10, 11, 12, 13 ]
 
 
 console.log((Array.isArray("Sagar")));  // false: its string
 console.log((Array.from("Sagar")));     // [ 'S', 'a', 'g', 'a', 'r' ] // convert into array
 
 console.log(Array.from({name:"sagar"})); // [] // array of what? key or value?
-console.log(Array.from({name:"sagar"})); // [] // array of what? key or value?
 
 var mark1 = 100;
 var mark2 = 80;
 var mark3 = 30;
-console.log(Array.of(mark1, mark2, mark3));
+console.log(Array.of(mark1, mark2, mark3)); // [ 100, 80, 30 ]
 
-console.log("Nullish Coalescing Operator (??): null undefined");
+console.log("Nullish Coalescing Operator (??): ignore null undefined");
 
 let val1 = 5 ?? 10; // 5
 val1 = null ?? 10;  // 10
@@ -378,7 +413,7 @@ console.log(val1);
 val1 = undefined || null || 15 || 10;   // 15 // same we can do using OR operator
 
 console.log("Difference between OR(||) and Nullish Coalescing Operator (??)");
-
+// Note: "||" ignore falsy values and "??" ignore null and undefined.
 val1 = undefined ?? null ?? 0 ?? 11;  // 0 // it only ignores undefined and null.
 console.log(val1);
 val1 = undefined || null || 0 || 11;  // 11 // it only ignores all false values.
@@ -387,4 +422,7 @@ console.log(val1);
 
 // multiline
 var isPresent = true
-if(isPresent) console.log("log1"), console.log("log2");;
+if(isPresent) console.log("log1"), console.log("log2"), isPresent = false, console.log(isPresent);  
+// log1
+// log2
+// false
